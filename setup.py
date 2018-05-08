389d26aac7dedcfd00554dfd6b1431a1c64de3e8
Matt Smith <soimafreak@gmail.com>
2018-05-08 11:17:55 +0100

    Version bump to see if it will distribute...

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='eventmagic',
-    version='0.1.11',
+    version='0.1.12',
     description='Event scheduling with persistence for short-lived processes \
 i.e. AWS Lambda',
     long_description=long_description,
